8242d1436a11bde1e058b7f9e15b1e5d0a84dbf1
arr09 <arrheniuspaelongan09@gmail.com>
2024-12-16 17:27:43 +0700

    Fix Sqli

--- admin/agt/edit_agt.php
+++ admin/agt/edit_agt.php
@@ -1,10 +1,13 @@
 <?php
-
-    if(isset($_GET['kode'])){
-        $sql_cek = "SELECT * FROM tb_anggota WHERE id_anggota='".$_GET['kode']."'";
-        $query_cek = mysqli_query($koneksi, $sql_cek);
-        $data_cek = mysqli_fetch_array($query_cek,MYSQLI_BOTH);
-    }
+if (isset($_GET['kode'])) {
+    $sql_cek = "SELECT * FROM tb_anggota WHERE id_anggota = ?";
+    $stmt = $koneksi->prepare($sql_cek);
+    $stmt->bind_param("s", $_GET['kode']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data_cek = $result->fetch_assoc();
+    $stmt->close();
+}
 ?>
 
 <section class="content-header">
@@ -25,57 +28,41 @@
 <section class="content">
 	<div class="row">
 		<div class="col-md-12">
-			<!-- general form elements -->
 			<div class="box box-success">
 				<div class="box-header with-border">
 					<h3 class="box-title">Ubah Anggota</h3>
 				</div>
-				<!-- /.box-header -->
-				<!-- form start -->
 				<form action="" method="post" enctype="multipart/form-data">
 					<div class="box-body">
-
 						<div class="form-group">
-							<label>Id anggota</label>
-							<input type='text' class="form-control" name="id_anggota" value="<?php echo $data_cek['id_anggota']; ?>"
-							 readonly/>
+							<label>Id Anggota</label>
+							<input type="text" class="form-control" name="id_anggota" value="<?php echo htmlspecialchars($data_cek['id_anggota']); ?>" readonly />
 						</div>
 
 						<div class="form-group">
 							<label>Nama</label>
-							<input type='text' class="form-control" name="nama" value="<?php echo $data_cek['nama']; ?>"
-							/>
+							<input type="text" class="form-control" name="nama" value="<?php echo htmlspecialchars($data_cek['nama']); ?>" />
 						</div>
 
 						<div class="form-group">
 							<label>Jenis Kelamin</label>
 							<select name="jekel" id="jekel" class="form-control" required>
 								<option value="">-- Pilih --</option>
-								<?php
-                                //cek data yg dipilih sebelumnya
-                                if ($data_cek['jekel'] == "Laki-laki") echo "<option value='Laki-laki' selected>Laki-laki</option>";
-                                else echo "<option value='Laki-laki'>Laki-laki</option>";
-                                
-                                if ($data_cek['jekel'] == "Perempuan") echo "<option value='Perempuan' selected>Perempuan</option>";
-                                else echo "<option value='Perempuan'>Perempuan</option>";
-                            ?>
+								<option value="Laki-laki" <?php echo ($data_cek['jekel'] == "Laki-laki" ? "selected" : ""); ?>>Laki-laki</option>
+								<option value="Perempuan" <?php echo ($data_cek['jekel'] == "Perempuan" ? "selected" : ""); ?>>Perempuan</option>
 							</select>
 						</div>
 
 						<div class="form-group">
 							<label>Kelas</label>
-							<input type='text' class="form-control" name="kelas" value="<?php echo $data_cek['kelas']; ?>"
-							/>
+							<input type="text" class="form-control" name="kelas" value="<?php echo htmlspecialchars($data_cek['kelas']); ?>" />
 						</div>
 
 						<div class="form-group">
 							<label>No HP</label>
-							<input type='number' class="form-control" name="no_hp" value="<?php echo $data_cek['no_hp']; ?>"
-							/>
+							<input type="number" class="form-control" name="no_hp" value="<?php echo htmlspecialchars($data_cek['no_hp']); ?>" />
 						</div>
-
 					</div>
-					<!-- /.box-body -->
 
 					<div class="box-footer">
 						<input type="submit" name="Ubah" value="Ubah" class="btn btn-success">
@@ -83,22 +70,18 @@
 					</div>
 				</form>
 			</div>
-			<!-- /.box -->
+		</div>
 </section>
 
 <?php
+if (isset($_POST['Ubah'])) {
+    // Proses ubah data dengan prepared statement
+    $sql_ubah = "UPDATE tb_anggota SET nama = ?, jekel = ?, kelas = ?, no_hp = ? WHERE id_anggota = ?";
+    $stmt = $koneksi->prepare($sql_ubah);
+    $stmt->bind_param("sssss", $_POST['nama'], $_POST['jekel'], $_POST['kelas'], $_POST['no_hp'], $_POST['id_anggota']);
+    $result = $stmt->execute();
 
-if (isset ($_POST['Ubah'])){
-    //mulai proses ubah
-    $sql_ubah = "UPDATE tb_anggota SET
-		nama='".$_POST['nama']."',
-		jekel='".$_POST['jekel']."',
-		kelas='".$_POST['kelas']."',
-        no_hp='".$_POST['no_hp']."'
-        WHERE id_anggota='".$_POST['id_anggota']."'";
-    $query_ubah = mysqli_query($koneksi, $sql_ubah);
-
-    if ($query_ubah) {
+    if ($result) {
         echo "<script>
         Swal.fire({title: 'Ubah Data Berhasil',text: '',icon: 'success',confirmButtonText: 'OK'
         }).then((result) => {
@@ -106,7 +89,7 @@ if (isset ($_POST['Ubah'])){
                 window.location = 'index.php?page=MyApp/data_agt';
             }
         })</script>";
-        }else{
+    } else {
         echo "<script>
         Swal.fire({title: 'Ubah Data Gagal',text: '',icon: 'error',confirmButtonText: 'OK'
         }).then((result) => {
@@ -115,5 +98,6 @@ if (isset ($_POST['Ubah'])){
             }
         })</script>";
     }
+    $stmt->close();
 }
-
+?>
--- admin/pengguna/edit_pengguna.php
+++ admin/pengguna/edit_pengguna.php
@@ -1,10 +1,13 @@
 <?php
-
-    if(isset($_GET['kode'])){
-        $sql_cek = "SELECT * FROM tb_pengguna WHERE id_pengguna='".$_GET['kode']."'";
-        $query_cek = mysqli_query($koneksi, $sql_cek);
-        $data_cek = mysqli_fetch_array($query_cek,MYSQLI_BOTH);
-    }
+if (isset($_GET['kode'])) {
+    $sql_cek = "SELECT * FROM tb_pengguna WHERE id_pengguna = ?";
+    $stmt = $koneksi->prepare($sql_cek);
+    $stmt->bind_param("s", $_GET['kode']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data_cek = $result->fetch_assoc();
+    $stmt->close();
+}
 ?>
 
 <section class="content-header">
@@ -24,7 +27,6 @@
 <section class="content">
 	<div class="row">
 		<div class="col-md-12">
-			<!-- general form elements -->
 			<div class="box box-success">
 				<div class="box-header with-border">
 					<h3 class="box-title">Ubah Pengguna</h3>
@@ -37,32 +39,26 @@
 						</button>
 					</div>
 				</div>
-				<!-- /.box-header -->
-				<!-- form start -->
 				<form action="" method="post" enctype="multipart/form-data">
 					<div class="box-body">
 
 						<div class="form-group">
-							<input type='hidden' class="form-control" name="id_pengguna" value="<?php echo $data_cek['id_pengguna']; ?>"
-							 readonly/>
+							<input type="hidden" class="form-control" name="id_pengguna" value="<?php echo htmlspecialchars($data_cek['id_pengguna']); ?>" readonly />
 						</div>
 
 						<div class="form-group">
 							<label>Nama Pengguna</label>
-							<input class="form-control" name="nama_pengguna" value="<?php echo $data_cek['nama_pengguna']; ?>"
-							/>
+							<input class="form-control" name="nama_pengguna" value="<?php echo htmlspecialchars($data_cek['nama_pengguna']); ?>" />
 						</div>
 
 						<div class="form-group">
 							<label>Username</label>
-							<input class="form-control" name="username" value="<?php echo $data_cek['username']; ?>"
-							/>
+							<input class="form-control" name="username" value="<?php echo htmlspecialchars($data_cek['username']); ?>" />
 						</div>
 
 						<div class="form-group">
 							<label for="exampleInputPassword1">Password</label>
-							<input type="password" class="form-control" name="password" id="pass" value="<?php echo $data_cek['password']; ?>"
-							/>
+							<input type="password" class="form-control" name="password" id="pass" placeholder="Kosongkan jika tidak ingin mengubah" />
 							<input id="mybutton" onclick="change()" type="checkbox" class="form-checkbox"> Lihat Password
 						</div>
 
@@ -70,21 +66,12 @@
 							<label>Level</label>
 							<select name="level" id="level" class="form-control" required>
 								<option value="">-- Pilih Level --</option>
-								<?php
-                            //menhecek data yg dipilih sebelumnya
-                           
-                            if ($data_cek['level'] == "Administrator") echo "<option value='Administrator' selected>Administrator</option>";
-                            else echo "<option value='Administrator'>Administrator</option>";
-
-                            if ($data_cek['level'] == "Petugas") echo "<option value='Petugas' selected>Petugas</option>";
-                            else echo "<option value='Petugas'>Petugas</option>";
-        
-                        ?>
+								<option value="Administrator" <?php echo ($data_cek['level'] == "Administrator" ? "selected" : ""); ?>>Administrator</option>
+								<option value="Petugas" <?php echo ($data_cek['level'] == "Petugas" ? "selected" : ""); ?>>Petugas</option>
 							</select>
 						</div>
 
 					</div>
-					<!-- /.box-body -->
 
 					<div class="box-footer">
 						<input type="submit" name="Ubah" value="Ubah" class="btn btn-success">
@@ -92,57 +79,59 @@
 					</div>
 				</form>
 			</div>
-			<!-- /.box -->
+		</div>
 </section>
 
 <?php
+if (isset($_POST['Ubah'])) {
+    // Proses update data dengan prepared statement
+    $sql_ubah = "UPDATE tb_pengguna SET nama_pengguna = ?, username = ?, level = ?";
 
-if (isset ($_POST['Ubah'])){
-    //mulai proses ubah
-    $sql_ubah = "UPDATE tb_pengguna SET
-        nama_pengguna='".$_POST['nama_pengguna']."',
-        username='".$_POST['username']."',
-        password='" . password_hash($_POST['password'], PASSWORD_BCRYPT) . "',
-        level='".$_POST['level']."'
-        WHERE id_pengguna='".$_POST['id_pengguna']."'";
-    $query_ubah = mysqli_query($koneksi, $sql_ubah);
-    if ($query_ubah) {
-        echo "<script>
-      Swal.fire({title: 'Ubah Data Berhasil',text: '',icon: 'success',confirmButtonText: 'OK'
-      }).then((result) => {
-          if (result.value) {
-              window.location = 'index.php?page=MyApp/data_pengguna';
-          }
-      })</script>";
-      }else{
-      echo "<script>
-      Swal.fire({title: 'Ubah Data Gagal',text: '',icon: 'error',confirmButtonText: 'OK'
-      }).then((result) => {
-          if (result.value) {
-              window.location = 'index.php?page=MyApp/data_pengguna';
-          }
-      })</script>";
+    // Jika password diisi, tambahkan ke query
+    if (!empty($_POST['password'])) {
+        $hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $sql_ubah .= ", password = ?";
+    }
+    $sql_ubah .= " WHERE id_pengguna = ?";
+
+    $stmt = $koneksi->prepare($sql_ubah);
+
+    // Bind parameter sesuai kondisi
+    if (!empty($_POST['password'])) {
+        $stmt->bind_param("sssss", $_POST['nama_pengguna'], $_POST['username'], $_POST['level'], $hashed_password, $_POST['id_pengguna']);
+    } else {
+        $stmt->bind_param("ssss", $_POST['nama_pengguna'], $_POST['username'], $_POST['level'], $_POST['id_pengguna']);
     }
 
-    //selesai proses ubah
+    // Eksekusi dan cek hasil
+    if ($stmt->execute()) {
+        echo "<script>
+        Swal.fire({title: 'Ubah Data Berhasil',text: '',icon: 'success',confirmButtonText: 'OK'
+        }).then((result) => {
+            if (result.value) {
+                window.location = 'index.php?page=MyApp/data_pengguna';
+            }
+        })</script>";
+    } else {
+        echo "<script>
+        Swal.fire({title: 'Ubah Data Gagal',text: '',icon: 'error',confirmButtonText: 'OK'
+        }).then((result) => {
+            if (result.value) {
+                window.location = 'index.php?page=MyApp/data_pengguna';
+            }
+        })</script>";
+    }
+    $stmt->close();
 }
-
 ?>
 
 <script type="text/javascript">
-        function change()
-        {
+    function change() {
         var x = document.getElementById('pass').type;
-
-        if (x == 'password')
-        {
+        if (x == 'password') {
             document.getElementById('pass').type = 'text';
-            document.getElementById('mybutton').innerHTML;
-        }
-        else
-        {
+        } else {
             document.getElementById('pass').type = 'password';
-            document.getElementById('mybutton').innerHTML;
         }
-        }
-    </script>
+    }
+</script>
